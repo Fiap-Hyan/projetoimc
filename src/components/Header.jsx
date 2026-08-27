af947d801f8d0f2cb370e1cd6a3cef59aa41b733
hyan <hyan.ha3@gmail.com>
2026-08-27 12:06:06 -0300

    fix: sobrescreve os jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "../css/header.css"
+import "../css/global.css"
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section className="header">
+        <h1>Calculadora IMC</h1>
+
+    </section>
+  )
+}
+
+export default Header
